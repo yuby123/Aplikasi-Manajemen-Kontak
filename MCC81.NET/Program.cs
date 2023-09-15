65dcb953b2fcf40301842923b2085d141fcd483d
Ahmad Sulthon Al Ayyubi <dafiqalimi17@gmail.com>
2023-09-15 14:05:57 +0700

    add class program

---
+++ MCC81.NET/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCC81.NET
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
