8fde170ac90f086fdb93e4013591fd19c351c213
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-15 15:24:35 +0700

    Update ContactManager.cs

--- MCC81.NET/ContactManager.cs
+++ MCC81.NET/ContactManager.cs
@@ -2,9 +2,188 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace MCC81.NET;
-internal class ContactManager
+namespace MCC81.NET
 {
+    public class ContactManager
+    {
+        // Menyimpan daftar kontak
+        private List<Contact> contacts = new List<Contact>();
+
+        // Implementasi Stack<T> yang digunakan untuk menampung kontak-kontak yang telah dihapus dari daftar dan dapat dilacak.
+        private Stack<Contact> deletedContacts = new Stack<Contact>();
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("== Pilih operasi==");
+                Console.WriteLine("1. Create Contact");
+                Console.WriteLine("2. View Contact");
+                Console.WriteLine("3. View Deleted");
+                Console.WriteLine("4. Search Contact");
+                Console.WriteLine("5. Exit");
+                Console.Write("Input :  ");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            CreateContact();
+                            break;
+
+                        case 2:
+                            ViewContacts();
+                            break;
+
+                        case 3:
+                            ViewDeletedContacts();
+                            break;
+
+                        case 4:
+                            SearchContact();
+                            break;
+
+                        case 5:
+                            return;
+
+                        default:
+                            Console.WriteLine("Pilihan tidak valid!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Masukkan pilihan yang valid.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+
+
+        private void ViewContacts()
+        {
+            Console.WriteLine("Daftar Kontak:");
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Nama: {contact.Name}, Telepon: {contact.PhoneNumber}, Email: {contact.EmailAddress}");
+            }
+        }
+
+        private void CreateContact()
+        {
+            Console.Clear();
+            Console.WriteLine("== Tambah Kontak ==\n");
+            Console.Write("Masukkan Nama: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Masukkan Nomor Telepon: ");
+            var phone = Console.ReadLine();
+            while (!Regex.IsMatch(phone, @"^\+?[0-9]{8,15}$"))
+            {
+                Console.WriteLine("Nomor telepon tidak valid! Masukkan lagi:");
+                phone = Console.ReadLine();
+            }
+
+            Console.Write("Masukkan Email: ");
+            var email = Console.ReadLine();
+            while (!Regex.IsMatch(email, @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$"))
+            {
+                Console.WriteLine("Email tidak valid! Masukkan lagi:");
+                email = Console.ReadLine();
+            }
+
+            var contact = new Contact(name, phone, email);
+            contacts.Add(contact);
+            Console.WriteLine("\nKontak berhasil ditambahkan.");
+            Console.WriteLine("Tekan Enter untuk kembali ke menu.");
+            Console.ReadLine();
+        }
+
+        private void UpdateContact()
+        {
+            Console.WriteLine("Masukkan nama kontak yang ingin diperbarui:");
+            var name = Console.ReadLine();
+
+            var contact = contacts.Find(c => c.Name == name);
+            if (contact == null)
+            {
+                Console.WriteLine("Kontak tidak ditemukan!");
+                return;
+            }
+
+            Console.WriteLine("Masukkan Nomor Telepon baru:");
+            var phone = Console.ReadLine();
+            while (!Regex.IsMatch(phone, @"^\+?[0-9]{8,15}$"))
+            {
+                Console.WriteLine("Nomor telepon tidak valid! Masukkan lagi:");
+                phone = Console.ReadLine();
+            }
+            contact.PhoneNumber = phone;
+
+            Console.WriteLine("Masukkan Email baru:");
+            var email = Console.ReadLine();
+            while (!Regex.IsMatch(email, @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$"))
+            {
+                Console.WriteLine("Email tidak valid! Masukkan lagi:");
+                email = Console.ReadLine();
+            }
+            contact.EmailAddress = email;
+        }
+
+        private void DeleteContact()
+        {
+            Console.WriteLine("Masukkan nama kontak yang ingin dihapus:");
+            var name = Console.ReadLine();
+
+            var contact = contacts.Find(c => c.Name == name);
+            if (contact == null)
+            {
+                Console.WriteLine("Kontak tidak ditemukan!");
+                return;
+            }
+
+            contacts.Remove(contact);
+            deletedContacts.Push(contact);
+            Console.WriteLine("Kontak telah dihapus.");
+        }
+
+        private void ViewDeletedContacts()
+        {
+            Console.WriteLine("Daftar Kontak yang Dihapus:");
+            foreach (var contact in deletedContacts)
+            {
+                Console.WriteLine($"Nama: {contact.Name}, Telepon: {contact.PhoneNumber}, Email: {contact.EmailAddress}");
+            }
+        }
+
+        private void SearchContact(string fullname)
+        {
+            // Mencari pengguna yang cocok berdasarkan nama yang mengandung kata kunci
+            var searchUser = contacts.Where(u => Regex.IsMatch(u.Name, fullname, RegexOptions.IgnoreCase)
+                                                 Regex.IsMatch(u.PhoneNumber, fullname, RegexOptions.IgnoreCase)
+                                                 Regex.IsMatch(u.EmailAddress, fullname, RegexOptions.IgnoreCase)).ToList();
+            if (searchUser.Count == 0)
+            {
+                Console.WriteLine("Tidak ada pengguna yang cocok dengan kata kunci yang diberikan !");
+            }
+            else
+            {
+                Console.WriteLine("--------------------------");
+                foreach (var item in searchUser)
+                {
+                    Console.WriteLine($"\nNama : {item.Name}");
+                    Console.WriteLine($"PhoneNumber : {item.PhoneNumber}");
+                    Console.WriteLine($"EmailAddress : {item.EmailAddress}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
 }
