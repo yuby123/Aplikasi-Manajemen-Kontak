df8de107d54efe59270d93ebf85b0cff952ad2c3
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-15 21:33:10 +0700

    Revert "Update method SearchContact"
    This reverts commit 929d9b1491b5d5866f181a635c36cd7848f502d3.

--- MCC81.NET/ContactManager.cs
+++ MCC81.NET/ContactManager.cs
@@ -189,7 +189,7 @@ namespace MCC81.NET
             //ViewContacts();
             Console.Clear();
             Console.WriteLine("== Cari Kontak ==\n");
-            Console.Write("Masukkan Nama / Nomor HP / Email: ");
+            Console.Write("Masukkan Nama/NoHP/Email : ");
             var name = Console.ReadLine();
 
             // Mencari pengguna yang cocok berdasarkan nama yang mengandung kata kunci
@@ -200,8 +200,6 @@ namespace MCC81.NET
             if (searchUser.Count == 0)
             {
                 Console.WriteLine("Tidak ada pengguna yang cocok dengan kata kunci yang diberikan !");
-                Console.ReadLine();
-                return;
             }
             else
             {
@@ -218,14 +216,10 @@ namespace MCC81.NET
             do
             {
                 Console.WriteLine("Menu");
-                if (searchUser.Count > 0)
-                {
-                    Console.WriteLine("1. Edit Kontak");
-                    Console.WriteLine("2. Hapus Kontak");
-                }
-                Console.WriteLine("3. Kembali ke Menu Awal");
+                Console.WriteLine("1. Edit Kontak");
+                Console.WriteLine("2. Hapus Kontak");
+                Console.WriteLine("3. Back");
                 Console.Write("Masukkan Pilihan: ");
-
                 if (int.TryParse(Console.ReadLine(), out choice))
                 {
                     switch (choice)
@@ -250,7 +244,7 @@ namespace MCC81.NET
 
         private void UpdateContact()
         {
-            Console.Write("Masukkan nama kontak yang ingin diperbarui: ");
+            Console.WriteLine("Masukkan nama kontak yang ingin diperbarui:");
             var name = Console.ReadLine();
 
             var contact = contacts.Find(c => c.Name == name);
@@ -281,7 +275,7 @@ namespace MCC81.NET
 
         private void DeleteContact()
         {
-            Console.Write("Masukkan nama kontak yang ingin dihapus: ");
+            Console.WriteLine("Masukkan nama kontak yang ingin dihapus:");
             var name = Console.ReadLine();
 
             var contact = contacts.Find(c => c.Name == name);
